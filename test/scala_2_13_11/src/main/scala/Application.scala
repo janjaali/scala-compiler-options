70d59e5910d8261b0269b50aebc3e7f5202253c9
Siyavash Habashi <ghashange@live.com>
2024-11-23 18:49:17 +0100

    Support for Scala 2.13.11.

---
+++ test/scala_2_13_11/src/main/scala/Application.scala
@@ -0,0 +1,17 @@
+object Application {
+
+  def main(args: Array[String]): Unit = {
+
+    // val someInt = 1
+    // val someUnit = ()
+
+    println("foo: " + {
+      // math.pow(2, (someInt / 2) : Double)
+      // someUnit.asInstanceOf[Int]
+      // 42.isNaN()
+      // f(42, 42)
+    })
+  }
+
+  // private def f(u: Unit) = u
+}

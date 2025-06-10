916f0f6ca03fcbe3533c94e67aa84c6d051c38e3
Siyavash Habashi <ghashange@live.com>
2025-06-10 20:52:26 +0200

    Fix release publishing.

--- project/publishSettings.scala
+++ project/publishSettings.scala
@@ -3,8 +3,4 @@ import sbt._
 object publishSettings {
 
   val credentials = Credentials(Path.userHome / ".sbt" / ".credentials")
-
-  val publishTo = Some {
-    "sonatype" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-  }
 }
